bd154787214ce3865aff8bd32af9ab288a7728b5
chalermporn17personal <chalermporn17.personal@gmail.com>
2020-10-12 12:57:32 +0700

    Fundamental Auth system

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Get , Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { AuthGuard } from '@nestjs/passport';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return null
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
+
+
+
+
+
