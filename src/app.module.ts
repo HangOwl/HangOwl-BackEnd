bbc682eb5666a95d6441048b8eedc78a380d2d48
chalermporn17personal <chalermporn17.personal@gmail.com>
2020-10-13 12:25:53 +0700

    remove constroller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { BarsController } from './bars/bars.constroller';
+import { BarsController } from './bars/bars.controller';
 import { BarsModule } from './bars/bars.module';
 @Module({
   imports: [AuthModule, UsersModule , BarsModule ],
--- src/bars/bars.service.ts
+++ src/bars/bars.service.ts
@@ -39,13 +39,26 @@ export class BarsService{
         bar.ProfilePicPath = ""
         bar.AdditionalPicture = []
         // Check if is true format
-        // const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        // return re.test(String(bar.Email).toLowerCase());
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if ( ! re.test(String(bar.Email).toLowerCase()) ) 
+          // Bad email format
+          return null
+        if ( bar.OpenTime.length != 4 || bar.CloseTime.length != 4 )
+          // Bad time format
+          return null
+        if ( typeof bar.CloseWeekDay != 'object' || bar.CloseWeekDay.length != 7 )
+          // Bad Close week Days format
+          return null
+        
+        //adding bar procedure
+
+        //email procedure
+        return bar
     }
     async bar_profile(id): Promise<Bar | undefined>
     {
         //return  bar that match with id
-        //bar_id is string
+        //id is string
         return this.bars.find(bar => bar.userId === id);
     }
 
