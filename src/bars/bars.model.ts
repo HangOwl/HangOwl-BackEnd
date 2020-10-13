06cf528e42d8e73b344ddecaa2f7342c89a13f45
OreganoTea <natt.nawarat@gmail.com>
2020-10-13 18:04:38 +0700

    get_all_id add find email

--- src/bars/bars.model.ts
+++ src/bars/bars.model.ts
@@ -1,27 +1,27 @@
 import * as mongoose from 'mongoose';
 
-export const BarSchema = new mongoose.Schema(
-    {
-        Email: {type: String, required: true},
-        Password: {type: String, required: true},
-        Salt: {type: String, required: true},
-        Role: {type: Number, required: true},
-        BarName: {type: String, required: true},
-        ProfilePicPath : String,
-        AdditionalPicPath : [String],
-        LineID : String,
-        OpenTime : String,
-        CloseTime : String,
-        CloseWeekDay : [Boolean],
-        Address: String,
-        AdminApproved : Boolean,
-        BarDescription : String,
-        BarRule : String,
-        Reservations: [String]
-
-    }
-    , { collection: 'Users' }
-)
+//export const BarSchema = new mongoose.Schema(
+//    {
+//        Email: {type: String, required: true},
+//        Password: {type: String, required: true},
+//        Salt: {type: String, required: true},
+//        Role: {type: Number, required: true},
+//        BarName: {type: String, required: true},
+//        ProfilePicPath : String,
+//        AdditionalPicPath : [String],
+//        LineID : String,
+//        OpenTime : String,
+//        CloseTime : String,
+//        CloseWeekDay : [Boolean],
+//        Address: String,
+//        AdminApproved : Boolean,
+//        BarDescription : String,
+//        BarRule : String,
+//        Reservations: [String]
+//
+//    }
+//    , { collection: 'Users' }
+//)
 
 export interface Bar extends mongoose.Document {
     id: string;
