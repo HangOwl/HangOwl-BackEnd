8740e884d72d6972cd7089b39c9f886ee4d39959
OreganoTea <natt.nawarat@gmail.com>
2020-10-13 14:53:58 +0700

    edit appmodule and barsmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,8 @@ import { UsersModule } from './users/users.module';
 import { BarsController } from './bars/bars.controller';
 import { BarsModule } from './bars/bars.module';
 @Module({
-  imports: [AuthModule, UsersModule , BarsModule ],
+  imports: [AuthModule, UsersModule , BarsModule,
+    MongooseModule.forRoot('mongodb+srv://DrunkOrca:I4XfDlPwPXpBjjXO@cluster0.t25ry.gcp.mongodb.net/HangOwl?retryWrites=true&w=majority'), ],
   controllers: [AppController , BarsController ],
   providers: [AppService],
 })
